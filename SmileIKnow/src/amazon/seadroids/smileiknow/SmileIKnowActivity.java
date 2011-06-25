03560def01e7479c4f9ebe3848ea019892d33f5f
maxgale <maxgale@amazon.com>
2011-06-24 20:19:31 -0700

    Test to push default project

---
+++ SmileIKnow/src/amazon/seadroids/smileiknow/SmileIKnowActivity.java
@@ -0,0 +1,13 @@
+package amazon.seadroids.smileiknow;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SmileIKnowActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
